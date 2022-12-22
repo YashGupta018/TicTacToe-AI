58761be017b44d5b94db03d3a154f590f73f81c5
YashGupta018 <ygupta2001@gmail.com>
2022-12-22 23:22:13 +0530

    Initial Commit

---
+++ tictactoe.py
@@ -0,0 +1,20 @@
+import sys
+import pygame
+
+from constants import *
+
+#PyGame
+pygame.init()
+screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
+pygame.display.set_caption('Tic Tac Toe AI')
+
+def main():
+
+    while True:
+        for event in pygame.event.get():
+            
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+main()
