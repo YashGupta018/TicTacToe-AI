d3c99a463412e7b97baa9ee3cf7c16aae1fdd918
YashGupta018 <ygupta2001@gmail.com>
2022-12-22 23:26:20 +0530

    Initial Commit

--- tictactoe.py
+++ tictactoe.py
@@ -12,7 +12,7 @@ def main():
 
     while True:
         for event in pygame.event.get():
-            
+
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
